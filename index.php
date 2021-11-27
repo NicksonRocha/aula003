1a470dc0604d3f61e2a29889e80ea1dff4d95a73
Nickson Rocha <nicksonrocha@hotmail.com>
2021-11-26 21:51:16 -0300

    atualização003

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
 
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/estilo.css?<?php echo mt_rand(1, 9) ?>">
     <link rel="stylesheet" href="css/bootstrap.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,16 +12,22 @@
 <body>
     <div class="box">    
         <h2 class="titulo">Conversor de temperatura</h2>
-        <label class="opção">temperatura em °C</label>
-        <input class="local" type="text" name="temperatura" id="temperatura" maxlength="4">
-        <label>para</label>
-        <select id="seleciona-temperatura"> 
+        <label class="opção">temperatura em</label>
+        <select id="seleciona-temperatura1"> 
             <option value="0">Kelvin</option>
             <option value="1">Fahrenheit</option>
+            <option value="2">Celsius</option>
+        </select>
+        <input class="local" type="text" name="temperatura" id="temperatura" maxlength="4">
+        <label>para</label>
+        <select id="seleciona-temperatura2"> 
+            <option value="0">Kelvin</option>
+            <option value="1">Fahrenheit</option>
+            <option value="2">Celsius</option>
         </select>
         <input class="botão" type="submit" name="converter" value="converter" id="converter" onclick="converter()">
-        <div id="resultado"></div>
-        <script type="text/javascript" src="js/javascript.js"></script>
+        <div id="resultado">Resultado</div>
+        <script type="text/javascript" src="js/javascript.js?<?php echo mt_rand(1, 100) ?>"></script>
     </div>
 
 </body>
--- js/javascript.js
+++ js/javascript.js
@@ -1,29 +1,74 @@
 
 function converter() {
-    let select = parseInt(document.getElementById('seleciona-temperatura').value);
-    let temperatura = document.getElementById('temperatura').value;
+    let select1 = parseInt(document.getElementById('seleciona-temperatura1').value);
+    let select2 = parseInt(document.getElementById('seleciona-temperatura2').value);
+    let temperatura = parseInt(document.getElementById('temperatura').value);
     let formulaF = (parseInt(temperatura) * 1.8) + 32;
     let formulaK = parseInt(temperatura) + 273;
     let resultado = document.getElementById('resultado');
-//     if(select == 0){
-//         resultado.innerHTML = (`${temperatura}°C em kelvin é ${formulaK}K`);
-//     }  else if(select == 1){
-//         resultado.innerHTML = (`${temperatura}°C em Farenheit é ${formulaF}°F`);
-//     }
-//     //resultado.innerHTML ='Sua temperatura é <strong> ' + calculo + '°F</strong>';
+    // let nome = '';
+
+    // if(select1 === 0 || select2 === 0){
+    //     let nome = 'Kelvin';
+    // } else if(select1 === 1 || select2 ===1){
+    //     let nome = '°Farenheit';
+    // } else if(select1 === 2 || select2 === 2){
+    //     let nome = '°Celsius';
+    // }
+
+    if(select1 === 0 && select2 === 0 || select1 === 1 && select2 === 1 || select1 === 2 && select2 === 2 ){
+        switch(select1 || select2){
+            case 0:
+                resultado.innerHTML = (`A temperatura em kelvin é ${temperatura}`)
+            break;    
+            case 1:
+                resultado.innerHTML = (`A temperatura em °Fahrenheit é ${temperatura}`)
+            break;    
+            case 2:
+                resultado.innerHTML = (`A temperatura em °Celsius é ${temperatura}`)
+            break;    
+        
+        }
 
 
-    switch(select){
-        case 0:
-            resultado.innerHTML = (`${temperatura}°C em kelvin é ${formulaK}K`);
-        break;
-        case 1:
-            resultado.innerHTML = (`${temperatura}°C em Farenheit é ${formulaF}°F`);
-        break;
-        default:
-            resultado.innerHTML = ('Erro');
-        break;
-    }
+
+
+        // if()
+            // resultado.innerHTML = (`A temperatura é ${temperatura}`)
+        // resultado.innerHTML = (`${temperatura}°C em kelvin é ${formulaK}K`);
+    }  else if(select1 === 0 && select2 ===1){
+        let formulaF = temperatura - 459.67;
+        resultado.innerHTML = (`${temperatura}K em fahrenheit é ${formulaF}°farenheit`)
+    }  else if(select1 === 0 && select2 ===2){
+        let formulaC = temperatura - 273.15;
+        resultado.innerHTML = (`${temperatura}K em Celsius é ${formulaC}°Celsius`)
+    }  else if(select1 === 1 && select2 ===0){
+        let formulak = (temperatura - 32) * 5/9 + 273.15;
+        resultado.innerHTML = (`${temperatura}°farenheit em Kelvin é ${formulak}K`)
+    }  else if(select1 === 1 && select2 ===2){
+        let formulaC = (temperatura - 32) * 5/9;
+        resultado.innerHTML = (`${temperatura}°farenheit em Celsius é ${formulaC}°Celsius`)
+    }  else if(select1 === 2 && select2 ===0){
+        let formulak = temperatura + 273.15;
+        resultado.innerHTML = (`${temperatura}°Celsius em Kelvin é ${formulak}K`)
+    }  else if(select1 === 2 && select2 ===1){
+        let formulaF = (temperatura * 9/5) + 32;
+        resultado.innerHTML = (`${temperatura}°Celsius em Farenheit é ${formulaF}°farenheit`)
+    } 
+    //resultado.innerHTML ='Sua temperatura é <strong> ' + calculo + '°F</strong>';
+
+
+    // switch(select1){
+    //     case 0:
+    //         resultado.innerHTML = (`${temperatura}°C em kelvin é ${formulaK}K`);
+    //     break;
+    //     case 1:
+    //         resultado.innerHTML = (`${temperatura}°C em Farenheit é ${formulaF}°F`);
+    //     break;
+    //     default:
+    //         resultado.innerHTML = ('Erro');
+    //     break;
+    // }
 
 
 } 
